8f6e93bc7be0937c17e0ce67ad0504df6525cb25
Azzybana Raccoon <121582001+Azzybana@users.noreply.github.com>
2025-02-06 19:17:05 -0500

    add some exploding bits, almost entire error handler, lol

---
+++ src/panic/panic.rs
@@ -0,0 +1,41 @@
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn explode(info: &PanicInfo) -> ! {
+    // Prefix the panic message with "_"
+    let message = format!("_{}", info);
+    // Call abort to halt execution
+    core::intrinsics::abort()
+}
+
+#[macro_export]
+macro_rules! panic_or {
+    ($reason:expr, $thing:expr) => {
+        match unsafe { $thing } {
+            Ok(val) => val,
+            Err(_) => $crate::panic::explode($reason),
+        }
+    };
+}
+
+pub trait OrPanic<T> {
+    fn or_panic(self, reason: &str) -> T;
+}
+
+impl<T, E> OrPanic<T> for Result<T, E> {
+    fn or_panic(self, reason: &str) -> T {
+        match self {
+            Ok(val) => val,
+            Err(_) => explode(reason),
+        }
+    }
+}
+
+impl<T> OrPanic<T> for Option<T> {
+    fn or_panic(self, reason: &str) -> T {
+        match self {
+            Some(val) => val,
+            None => explode(reason),
+        }
+    }
+}
